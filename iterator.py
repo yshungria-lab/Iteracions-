61b8cb64d1e271b92f5b57b21ce74d4c5bd5adfe
Yojan Stiben Hungría <69788548+yshungria-lab@users.noreply.github.com>
2020-09-13 12:40:33 -0500

    Create iterator.py

---
+++ iterator.py
@@ -0,0 +1,6 @@
+iter('cadena') # cadena
+iter(['a', 'b', 'c']) # lista
+iter(('a', 'b', 'c')) # tupla
+iter({'a', 'b', 'c'}) # conjunto
+iter({'a': 1, 'b': 2, 'c': 3}) # diccionario
+Todas las llamadas anteriores regresan un objeto de tipo iterator.
